f432c547e87e35ece30b1c64ff254419814f8e37
ckol <christopher.kollar@ucalgary.ca>
2025-12-28 14:30:03 -0700

    implemented vectorization of title in wiki page

--- src/main/WikiPage.py
+++ src/main/WikiPage.py
@@ -1,12 +1,14 @@
+from sentence_transformers import SentenceTransformer
+from time import perf_counter
+
 class WikiPage:
     # init method must have access to needed elements to build object both from web scraping, but also from json reading
     # id is url, title is title stripped of - Wikipedia suffix, links is array of all 
-    def __init__(self, id, title, links): 
+    def __init__(self, id, title, links, title_vector): 
         self.id = id
         self.title = title       
         self.links = list(links) # convert from set (how its loaded) to list
-        
-        ...
+        self.title = title_vector
 
     # override __eq__ method 
 
@@ -33,6 +35,18 @@ class WikiPage:
                 continue
 
             links.add("https://en.wikipedia.org" + href_txt)
+
+        # get title vector       
+
+        #DEBUG time to vectorize
+
+        start_time = perf_counter()
+        model = SentenceTransformer('all-MiniLM-L6-v2')
+        title_vector = model.encode(title_text) 
         
+        end_time = perf_counter()   
+        elapsed_time = end_time - start_time
+        print(f"Time taken to vectorize title '{title_text}': {elapsed_time:.4f} seconds")
+
         # return
-        return url, title_text, links
+        return url, title_text, links, title_vector
--- src/main/main.py
+++ src/main/main.py
@@ -2,7 +2,7 @@ from scraper import generate_wiki_graph
 
 def main():
     START = "https://en.wikipedia.org/wiki/Ankara"
-    LIMIT = 2
+    LIMIT = 1
     wg = generate_wiki_graph(START, LIMIT)
     print(wg.graph)
     print(len(wg.graph))
@@ -10,7 +10,7 @@ def main():
     for page in wg.graph:
         print(page.title)
         print(page.id)
-
+        print(page.title_vector)
 
 
 
